6c2e3b758debf3e4f3e86a66d64d4215dd19ecd4
AHalic <sophie.gama@aluno.ufes.br>
2023-01-21 23:30:18 -0300

    :sparkles: receives user and updates profile info

--- app/src/main/java/com/example/katify/view/KanbansPageActivity.kt
+++ app/src/main/java/com/example/katify/view/KanbansPageActivity.kt
@@ -1,17 +1,35 @@
 package com.example.katify.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.katify.data.User
 import com.example.katify.databinding.ActivityKanbansPageBinding
+import com.squareup.picasso.Picasso
+
 
 class KanbansPageActivity : AppCompatActivity() {
     private lateinit var binding : ActivityKanbansPageBinding
+    private lateinit var user : User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityKanbansPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        user = getUserFromIntent()
+
+        binding.emailProfile.text = user.userEmail
+        binding.nameProfile.text = user.name
+
+        Picasso.get().load(Uri.parse(user.urlImage)).into(binding.iconImage)
+        Log.w("icon", user.urlImage)
+
         supportActionBar?.hide()
+    }
+
+    private fun getUserFromIntent(): User {
+        return intent.getSerializableExtra("user") as User
     }
 }
