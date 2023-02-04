861157b143c0c411bcd303801d56e3d9b213b409
AHalic <sophie.gama@aluno.ufes.br>
2023-02-03 23:17:10 -0300

    :sparkles: adds listener to navigate back and passes data to start point

--- app/src/main/java/com/example/katify/view/SelectedKanbanActivity.kt
+++ app/src/main/java/com/example/katify/view/SelectedKanbanActivity.kt
@@ -2,14 +2,20 @@ package com.example.katify.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.katify.R
 import com.example.katify.data.model.Kanban
 import com.example.katify.databinding.ActivitySelectedKanbanBinding
+import com.example.katify.viewModel.KanbanViewModel
 
-class SelectedKanbanActivity : AppCompatActivity() {
+class SelectedKanbanActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding : ActivitySelectedKanbanBinding;
     private var kanban : Int = -1
     private lateinit var navController: NavController
@@ -23,8 +29,26 @@ class SelectedKanbanActivity : AppCompatActivity() {
         Toast.makeText(this, "$kanban", Toast.LENGTH_SHORT).show()
         supportActionBar?.hide()
 
+        val bundle = bundleOf("kanban_id" to kanban)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<SelectedKanbanFragment>(R.id.fragment_container_view, args=bundle)
+        }
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         navController = navHostFragment.navController
+
+
+        binding.backArrow.setOnClickListener(this)
+    }
+
+    override fun onClick (view: View) {
+//        val lastAddedFragmentId = navController.currentDestination?.id ?: return
+
+        if (view.id == R.id.back_arrow) {
+            finish()
+        }
     }
 
     private fun getKanbanFromIntent(): Int {
