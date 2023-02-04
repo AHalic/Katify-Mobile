d08fea6f89b2348c2b110ff435bf7e52df4060ff
AHalic <sophie.gama@aluno.ufes.br>
2023-02-03 23:16:40 -0300

    :sparkles: adds listener to kanban fragment

--- app/src/main/java/com/example/katify/view/SelectedKanbanFragment.kt
+++ app/src/main/java/com/example/katify/view/SelectedKanbanFragment.kt
@@ -1,60 +1,68 @@
 package com.example.katify.view
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.example.katify.R
+import com.example.katify.databinding.FragmentSelectedKanbanBinding
+import com.example.katify.utils.Constants
+import com.example.katify.viewModel.NoteViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SelectedKanbanFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SelectedKanbanFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class SelectedKanbanFragment : Fragment(R.layout.fragment_selected_kanban), View.OnClickListener {
+    private var _binding: FragmentSelectedKanbanBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var noteVM: NoteViewModel
+    val args: SelectedKanbanFragmentArgs by navArgs()
+    private lateinit var contextFrg: Context
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selected_kanban, container, false)
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        _binding = FragmentSelectedKanbanBinding.inflate(inflater, container, false)
+        noteVM = ViewModelProvider(requireActivity())[NoteViewModel::class.java]
+        setObserver()
+        binding.addNoteBtn.setOnClickListener(this)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectedKanbanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelectedKanbanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onClick(view: View) {
+        if (view.id == R.id.add_note_btn) {
+            noteVM.addNote(args.kanbanId)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    override fun onAttach(context: Context) {
+        contextFrg = context
+        super.onAttach(contextFrg)
+    }
+
+    private fun setObserver() {
+        noteVM.getIsAdded().observe(viewLifecycleOwner, Observer {
+            if (it == Constants.BD_MSGS.SUCCESS){
+                Toast.makeText(contextFrg, R.string.success_create, Toast.LENGTH_SHORT).show()
+            } else if (it == Constants.BD_MSGS.CONSTRAINT){
+                Toast.makeText(contextFrg, R.string.fail_search, Toast.LENGTH_SHORT).show()
             }
+        })
     }
 }
