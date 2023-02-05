fd8c62ad1feb367e00212448b650a18091c70afb
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 10:45:29 -0300

    :recycle: removes unused import

--- app/src/main/java/com/example/katify/view/KanbansPageActivity.kt
+++ app/src/main/java/com/example/katify/view/KanbansPageActivity.kt
@@ -7,12 +7,9 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.katify.R
 import com.example.katify.data.model.Kanban
@@ -39,23 +36,17 @@ class KanbansPageActivity : AppCompatActivity(), View.OnClickListener {
 
         user = getUserFromIntent()
 
+        // Defines user info
         binding.emailProfile.text = user.userEmail
         binding.nameProfile.text = user.name
 
+        // Gets user's google pic
         Picasso.get().load(Uri.parse(user.urlImage)).into(binding.iconImage)
         Log.w("icon", user.urlImage)
 
+        // Recycler View config
         binding.kanbansRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.kanbansRecyclerView.adapter = adapter
-
-        listVM = ViewModelProvider(this).get(KanbanViewModel::class.java)
-
-        supportActionBar?.hide()
-
-        listVM.getAllKanbansOfOwner(user.userId)
-        setObserver()
-
-        binding.addKanbanBtn.setOnClickListener(this)
 
         val listener = object : OnKanbanListener {
             override fun onClick(kanban: Kanban) {
@@ -63,11 +54,28 @@ class KanbansPageActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
         adapter.setListener(listener)
+
+
+        listVM = ViewModelProvider(this).get(KanbanViewModel::class.java)
+
+        supportActionBar?.hide()
+
+        setObserver()
+
+        binding.addKanbanBtn.setOnClickListener(this)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        listVM.getAllKanbansOfOwner(user.userId)
+
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.add_kanban_btn) {
-            listVM.addKanban(user.userId)
+            listVM.addKanban(user.userId, resources.getString(R.string.kanban_name_default))
         }
     }
 
@@ -83,25 +91,24 @@ class KanbansPageActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun setObserver() {
         listVM.getListMsg().observe(this, Observer {
-            if (it == Constants.BD_MSGS.SUCCESS){
-                Toast.makeText(this, R.string.success_search, Toast.LENGTH_SHORT).show()
-            } else if (it == Constants.BD_MSGS.NOT_FOUND){
+            if (it == Constants.BD_MSGS.NOT_FOUND){
                 Toast.makeText(this, R.string.not_found_search, Toast.LENGTH_SHORT).show()
-            } else {
+            } else if (it == Constants.BD_MSGS.FAIL) {
                 Toast.makeText(this, R.string.fail_search, Toast.LENGTH_SHORT).show()
             }
         })
 
         listVM.getKanbanList().observe(this, Observer {
-            adapter.updateProdList(it)
+            adapter.updateKanbanList(it)
         })
 
         listVM.getIsAdded().observe(this, Observer {
             if (it == Constants.BD_MSGS.SUCCESS){
-                Toast.makeText(this, R.string.success_create, Toast.LENGTH_SHORT).show()
+                listVM.getAllKanbansOfOwner(user.userId)
             } else if (it == Constants.BD_MSGS.CONSTRAINT){
                 Toast.makeText(this, R.string.fail_search, Toast.LENGTH_SHORT).show()
             }
         })
+
     }
 }
