5346eecd1a8a691667c17cdadbfc525d4d829945
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 17:12:36 -0300

    :recycle: changes delete return

--- app/src/main/java/com/example/katify/data/dao/KanbanDao.kt
+++ app/src/main/java/com/example/katify/data/dao/KanbanDao.kt
@@ -15,18 +15,8 @@ interface KanbanDao {
     fun addKanban(kanban: Kanban):Long
 
     @Delete
-    fun deleteKanban(kanban: Kanban):Int
+    fun deleteKanban(kanban: Kanban)
 
     @Query("UPDATE kanban SET name = :name WHERE id =:id")
     fun updateKanban(name: String?, id: Int)
-
-
-//    @Query("SELECT * FROM kanban WHERE growZoneNumber = :growZoneNumber ORDER BY name")
-//    fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): Flow<List<Plant>>
-//
-//    @Query("SELECT * FROM kanban WHERE id = :plantId")
-//    fun getPlant(plantId: String): Flow<Plant>
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    suspend fun insertAll(plants: List<Plant>)
 }
