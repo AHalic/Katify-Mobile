fb9f96cd6717518d80937b5eea8db908cf317e91
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 17:13:50 -0300

    :coffin: deletes unused imports

--- app/src/main/java/com/example/katify/view/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/katify/view/adapter/NoteAdapter.kt
@@ -3,16 +3,13 @@ package com.example.katify.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.katify.data.model.Kanban
-import com.example.katify.data.model.KanbanNotes
 import com.example.katify.data.model.Note
 import com.example.katify.databinding.CardNoteBinding
 import com.example.katify.view.listener.OnNoteListener
 import com.example.katify.view.viewHolder.NoteViewHolder
-import kotlinx.coroutines.flow.Flow
 
 class NoteAdapter : RecyclerView.Adapter<NoteViewHolder>() {
-    private var noteList: List<Note> = listOf()
+    var noteList: List<Note> = listOf()
     private lateinit var listener: OnNoteListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
