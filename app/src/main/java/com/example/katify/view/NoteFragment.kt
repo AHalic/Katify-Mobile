59cc6d7f637b7be44388db6b87eac4a8a30fb4f1
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 17:17:00 -0300

    :sparkles: adds text change listener

--- app/src/main/java/com/example/katify/view/NoteFragment.kt
+++ app/src/main/java/com/example/katify/view/NoteFragment.kt
@@ -2,47 +2,69 @@ package com.example.katify.view
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.katify.R
 import com.example.katify.databinding.FragmentNoteBinding
-import com.example.katify.databinding.FragmentSelectedKanbanBinding
-import com.example.katify.databinding.NoteContentViewBinding
-import com.example.katify.view.adapter.NoteAdapter
-import com.example.katify.viewModel.KanbanViewModel
+import com.example.katify.utils.Constants
 import com.example.katify.viewModel.NoteViewModel
 
-class NoteFragment : Fragment(R.layout.fragment_note), View.OnClickListener {
+class NoteFragment : Fragment(R.layout.fragment_note) {
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
     private lateinit var noteVM: NoteViewModel
     val args: NoteFragmentArgs by navArgs()
     private lateinit var contextFrg: Context
-    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        container?.removeAllViews()
         // Inflate the layout for this fragment
         _binding = FragmentNoteBinding.inflate(inflater, container, false)
         noteVM = ViewModelProvider(requireActivity())[NoteViewModel::class.java]
 
-        return binding.root
-    }
+        setObserver()
 
-    override fun onClick(view: View) {
-        if (view.id == R.id.back_arrow) {
-            findNavController().popBackStack()
-        }
+        binding.noteText.setText(args.note.content)
+        binding.noteNameInput.setText(args.note.note_name)
+
+        binding.noteNameInput.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                noteVM.updateNoteName(args.note.note_id, s.toString())
+            }
+        })
+
+        binding.noteText.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                noteVM.updateNoteDesc(args.note.note_id, s.toString())
+            }
+        })
+
+        return binding.root
     }
 
     override fun onAttach(context: Context) {
@@ -53,5 +75,20 @@ class NoteFragment : Fragment(R.layout.fragment_note), View.OnClickListener {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setObserver() {
+        noteVM.getIsDescUpdated().observe(viewLifecycleOwner) {
+            if (it == Constants.BD_MSGS.CONSTRAINT) {
+                Toast.makeText(contextFrg, R.string.fail_search, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        noteVM.getIsNameUpdated().observe(viewLifecycleOwner) {
+            if (it == Constants.BD_MSGS.CONSTRAINT) {
+                Toast.makeText(contextFrg, R.string.fail_search, Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
