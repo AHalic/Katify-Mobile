8b08aa248cecd3ba55fe5a0b51f2c3bc9698b00c
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 10:44:02 -0300

    :sparkles: adds dao update querys

--- app/src/main/java/com/example/katify/data/dao/KanbanDao.kt
+++ app/src/main/java/com/example/katify/data/dao/KanbanDao.kt
@@ -8,14 +8,17 @@ interface KanbanDao {
     @Query("SELECT * FROM kanban WHERE owner_id == :owner_id ORDER BY name")
     fun getKanbans(owner_id: String): List<Kanban>
 
+    @Query("SELECT * FROM kanban WHERE id == :id")
+    fun getKanban(id: Int): Kanban
+
     @Insert
     fun addKanban(kanban: Kanban):Long
 
     @Delete
     fun deleteKanban(kanban: Kanban):Int
 
-    @Update
-    fun updateKanban(kanban: Kanban)
+    @Query("UPDATE kanban SET name = :name WHERE id =:id")
+    fun updateKanban(name: String?, id: Int)
 
 
 //    @Query("SELECT * FROM kanban WHERE growZoneNumber = :growZoneNumber ORDER BY name")
--- app/src/main/java/com/example/katify/data/dao/NoteDao.kt
+++ app/src/main/java/com/example/katify/data/dao/NoteDao.kt
@@ -16,8 +16,8 @@ interface NoteDao {
      * the object mapping.
      */
     @Transaction
-    @Query("SELECT * FROM kanban WHERE id == :id AND id IN (SELECT DISTINCT(kanban_id) FROM note)")
-    fun getKanbanNotes(id: Int): Flow<List<KanbanNotes>>
+    @Query("SELECT * FROM note WHERE kanban_id == :id AND kanban_id IN (SELECT DISTINCT(id) FROM kanban)")
+    fun getKanbanNotes(id: Int): List<Note>
 
     @Insert
     fun saveNote(note: Note):Long
@@ -25,7 +25,10 @@ interface NoteDao {
     @Delete
     fun deleteNote(note: Note)
 
-    @Update
-    fun updateNote(note: Note)
+    @Query("UPDATE note SET note_name = :name WHERE id =:id")
+    fun updateNoteName(name: String?, id: Int)
+
+    @Query("UPDATE note SET content = :desc WHERE id =:id")
+    fun updateNoteDesc(desc: String?, id: Int)
 
 }
