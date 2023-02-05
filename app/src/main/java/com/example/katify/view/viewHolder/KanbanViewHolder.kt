395f95bdcf9ef3df1f415bc82acb4b45d274b716
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 10:45:41 -0300

    :recycle: removes unused import

--- app/src/main/java/com/example/katify/view/viewHolder/KanbanViewHolder.kt
+++ app/src/main/java/com/example/katify/view/viewHolder/KanbanViewHolder.kt
@@ -1,13 +1,8 @@
 package com.example.katify.view.viewHolder
 
-import android.content.Intent
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.katify.data.model.Kanban
-import com.example.katify.data.model.User
 import com.example.katify.databinding.CardKanbanBinding
-import com.example.katify.view.KanbansPageActivity
-import com.example.katify.view.SelectedKanbanActivity
 import com.example.katify.view.listener.OnKanbanListener
 
 class KanbanViewHolder(private val binding: CardKanbanBinding, private val listener: OnKanbanListener) : RecyclerView.ViewHolder(binding.root) {
