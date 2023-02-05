70d6872ef1cd0b760b93d02440ace8f0a4f386c3
AHalic <sophie.gama@aluno.ufes.br>
2023-02-05 17:20:21 -0300

    :coffin: removes unused import

--- app/src/main/java/com/example/katify/view/SelectedKanbanActivity.kt
+++ app/src/main/java/com/example/katify/view/SelectedKanbanActivity.kt
@@ -3,20 +3,16 @@ package com.example.katify.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.katify.R
-import com.example.katify.data.model.Kanban
 import com.example.katify.databinding.ActivitySelectedKanbanBinding
-import com.example.katify.viewModel.KanbanViewModel
 
 class SelectedKanbanActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding : ActivitySelectedKanbanBinding;
+    private lateinit var binding : ActivitySelectedKanbanBinding
     private var kanban : Int = -1
     private lateinit var navController: NavController
 
@@ -26,7 +22,6 @@ class SelectedKanbanActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         kanban = getKanbanFromIntent()
-        Toast.makeText(this, "$kanban", Toast.LENGTH_SHORT).show()
         supportActionBar?.hide()
 
         val bundle = bundleOf("kanban_id" to kanban)
@@ -39,13 +34,10 @@ class SelectedKanbanActivity : AppCompatActivity(), View.OnClickListener {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         navController = navHostFragment.navController
 
-
         binding.backArrow.setOnClickListener(this)
     }
 
     override fun onClick (view: View) {
-//        val lastAddedFragmentId = navController.currentDestination?.id ?: return
-
         if (view.id == R.id.back_arrow) {
             finish()
         }
